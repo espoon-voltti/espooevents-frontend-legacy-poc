44b1242e14da6894f164bf0b5ab22e59637c581c
Lauri Nuotio <lauri.nuotio@siili.com>
2019-10-09 14:36:23 +0300

    feat: add end-to-end tests for entering a new event

---
+++ test/EventConfirm.js
@@ -0,0 +1,14 @@
+import {
+    Selector
+} from 'testcafe'
+
+fixture `Try to search for the event, expects a sample entry`
+    .page `https://linkedevents.hel.fi/search`
+
+test('Try searching for the event', async t => {
+    await t
+        .expect(Selector('.Select-menu-outer').child(0).child(0).innerText).contains('Sellon kirjasto')
+        .typeText(Selector('.form-control'), 'Esimerkkiotsikko')
+        .click(Selector('.mui-raised-button'))
+        .expect(Selector('.name').nth(0).innerText, 'Esimerkkiotsikko')
+})
