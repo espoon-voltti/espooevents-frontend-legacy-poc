38139d6ed3ec8706720777f3901e5058f92cb1f7
Markus Kuosmanen <markus.kuosmanen@siili.com>
2019-10-14 12:02:03 +0300

    refactor: delete Helsinki passport auth

--- server/auth.js
+++ server/auth.js
@@ -1,44 +1,6 @@
-import {Passport} from 'passport'
-import HelsinkiStrategy from 'passport-helsinki'
-import _debug from 'debug'
-
-const debug = _debug('auth')
 const basicAuth = require('express-basic-auth')
 
-export function getPassport(settings) {
-    const passport = new Passport()
-
-    const helsinkiStrategy = new HelsinkiStrategy(
-        {
-            clientID: settings.helsinkiAuthId,
-            clientSecret: settings.helsinkiAuthSecret,
-            callbackURL: settings.publicUrl + '/auth/login/helsinki/return',
-        },
-        (accessToken, refreshToken, profile, done) => {
-            debug('access token:', accessToken)
-            debug('refresh token:', refreshToken)
-            debug('acquiring token from api...')
-
-            helsinkiStrategy.getAPIToken(
-                accessToken,
-                settings.helsinkiTargetApp,
-                token => {
-                    profile.token = token
-                    return done(null, profile)
-                }
-            )
-        }
-    )
-
-    passport.use(helsinkiStrategy)
-
-    passport.serializeUser((user, done) => done(null, user))
-    passport.deserializeUser((user, done) => done(null, user))
-
-    return passport
-}
-
-export function addAuth(server, passport, settings) {
+export function addAuth(server) {
     server.get(
         '/auth/login/helsinki',
         basicAuth({
