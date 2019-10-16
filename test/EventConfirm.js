73ab8dc7be0db464120a8b57317733849a775470
Markus Kuosmanen <markus.kuosmanen@siili.com>
2019-10-16 13:57:14 +0300

    style: format test files

--- test/EventConfirm.js
+++ test/EventConfirm.js
@@ -1,13 +1,16 @@
-import {
-    Selector
-} from 'testcafe'
+import {Selector} from 'testcafe'
 
-fixture `Try to search for the event, expects a sample entry`
-    .page `https://linkedevents.hel.fi/search`
+fixture`Try to search for the event, expects a sample entry`
+    .page`https://linkedevents.hel.fi/search`
 
 test('Try searching for the event', async t => {
     await t
-        .expect(Selector('.Select-menu-outer').child(0).child(0).innerText).contains('Sellon kirjasto')
+        .expect(
+            Selector('.Select-menu-outer')
+                .child(0)
+                .child(0).innerText
+        )
+        .contains('Sellon kirjasto')
         .typeText(Selector('.form-control'), 'Esimerkkiotsikko')
         .click(Selector('.mui-raised-button'))
         .expect(Selector('.name').nth(0).innerText, 'Esimerkkiotsikko')
--- test/EventInput.js
+++ test/EventInput.js
@@ -1,9 +1,6 @@
-import {
-    Selector
-} from 'testcafe'
+import {Selector} from 'testcafe'
 
-fixture `Getting Started`
-    .page `https://linkedevents.hel.fi/event/create/new`
+fixture`Getting Started`.page`https://linkedevents.hel.fi/event/create/new`
 
 test('Type out and save an event', async t => {
     await t
@@ -16,37 +13,132 @@ test('Type out and save an event', async t => {
 
         // Basic information
         .typeText(Selector('.hel-text-field').child(1), 'Esimerkkiotsikko')
-        .typeText(Selector('.hel-text-field').nth(1).child(1), 'Esimerkkikuvaus, lyhyt')
-        .typeText(Selector('.hel-text-field').nth(2).child(1), 'Esimerkkikuvaus, pitkä')
-        .typeText(Selector('.hel-text-field').nth(3).child(1), 'www.kotisivu.fi')
-        .typeText(Selector('.hel-text-field').nth(4).child(1), 'Tapahtuman järjestäjä')
+        .typeText(
+            Selector('.hel-text-field')
+                .nth(1)
+                .child(1),
+            'Esimerkkikuvaus, lyhyt'
+        )
+        .typeText(
+            Selector('.hel-text-field')
+                .nth(2)
+                .child(1),
+            'Esimerkkikuvaus, pitkä'
+        )
+        .typeText(
+            Selector('.hel-text-field')
+                .nth(3)
+                .child(1),
+            'www.kotisivu.fi'
+        )
+        .typeText(
+            Selector('.hel-text-field')
+                .nth(4)
+                .child(1),
+            'Tapahtuman järjestäjä'
+        )
 
         // Date picker
-        .typeText(Selector('.hel-text-field').nth(6).child(0).child(0), '01.01.2019')
-        .typeText(Selector('.hel-text-field').nth(7).child(1), '12:00')
-        .typeText(Selector('.hel-text-field').nth(8).child(0).child(0), '02.01.2019')
-        .typeText(Selector('.hel-text-field').nth(9).child(1), '18:00')
+        .typeText(
+            Selector('.hel-text-field')
+                .nth(6)
+                .child(0)
+                .child(0),
+            '01.01.2019'
+        )
+        .typeText(
+            Selector('.hel-text-field')
+                .nth(7)
+                .child(1),
+            '12:00'
+        )
+        .typeText(
+            Selector('.hel-text-field')
+                .nth(8)
+                .child(0)
+                .child(0),
+            '02.01.2019'
+        )
+        .typeText(
+            Selector('.hel-text-field')
+                .nth(9)
+                .child(1),
+            '18:00'
+        )
 
         // Premises
-        .typeText(Selector('.hel-select').child(1).child(0).child(0).child(1).child(0), 'Sellon kirjasto', {
-            timeout: 20000
-        })
-        .expect(Selector('.Select-menu-outer').child(0).child(0).innerText).contains('Sellon kirjasto')
+        .typeText(
+            Selector('.hel-select')
+                .child(1)
+                .child(0)
+                .child(0)
+                .child(1)
+                .child(0),
+            'Sellon kirjasto',
+            {
+                timeout: 20000,
+            }
+        )
+        .expect(
+            Selector('.Select-menu-outer')
+                .child(0)
+                .child(0).innerText
+        )
+        .contains('Sellon kirjasto')
         // .click(Selector('.Select-menu-outer').child(0).child(0).innerText).contains('Sellon kirjasto')
         .wait(10000)
-        .typeText(Selector('.hel-text-field').nth(11).child(1), 'Paikan lisätiedot')
+        .typeText(
+            Selector('.hel-text-field')
+                .nth(11)
+                .child(1),
+            'Paikan lisätiedot'
+        )
 
         // Social media urls
-        .typeText(Selector('.hel-text-field').nth(12).child(1), 'www.facebook.com/tapahtuma')
-        .typeText(Selector('.hel-text-field').nth(13).child(1), 'www.twitter.com/tapahtuma')
-        .typeText(Selector('.hel-text-field').nth(14).child(1), 'www.instagram.com/tapahtuma')
+        .typeText(
+            Selector('.hel-text-field')
+                .nth(12)
+                .child(1),
+            'www.facebook.com/tapahtuma'
+        )
+        .typeText(
+            Selector('.hel-text-field')
+                .nth(13)
+                .child(1),
+            'www.twitter.com/tapahtuma'
+        )
+        .typeText(
+            Selector('.hel-text-field')
+                .nth(14)
+                .child(1),
+            'www.instagram.com/tapahtuma'
+        )
 
         // Classification
-        .typeText(Selector('.hel-select').nth(1).child(1).child(0).child(0).child(1).child(0), 'kulttuuritapahtumat', {
-            timeout: 20000
-        })
-        .expect(Selector('.Select-menu-outer').child(0).child(0).innerText).contains('kulttuuritapahtumat')
-        .click(Selector('.Select-menu-outer').child(0).child(0))
+        .typeText(
+            Selector('.hel-select')
+                .nth(1)
+                .child(1)
+                .child(0)
+                .child(0)
+                .child(1)
+                .child(0),
+            'kulttuuritapahtumat',
+            {
+                timeout: 20000,
+            }
+        )
+        .expect(
+            Selector('.Select-menu-outer')
+                .child(0)
+                .child(0).innerText
+        )
+        .contains('kulttuuritapahtumat')
+        .click(
+            Selector('.Select-menu-outer')
+                .child(0)
+                .child(0)
+        )
 
         // Categories 7
         .click(Selector('.hel-checkbox').nth(7))
