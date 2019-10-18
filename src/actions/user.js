a734deffa89589b1bd27360968e0f43268641d48
Lauri Nuotio <lauri.nuotio@siili.com>
2019-10-18 14:05:20 +0300

    feat: add simple but decent looking login form

--- src/actions/user.js
+++ src/actions/user.js
@@ -83,36 +83,95 @@ export function retrieveUserFromSession() {
 export function login() {
     return dispatch => {
         return new Promise(resolve => {
-            const user = window.prompt('Käyttäjätunnus: ')
-            const password = window.prompt('Salasana: ')
-            console.log(user, password)
-            const credentials = user + ':' + password
-            const encodedCredentials = window.btoa(credentials)
-            fetch('/auth/login/helsinki', {
-                // method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                mode: 'cors', // no-cors, *cors, same-origin
-                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: 'same-origin', // include, *same-origin, omit
-                headers: {
-                    Authorization: 'Basic ' + encodedCredentials,
-                },
-                redirect: 'follow', // manual, *follow, error
-                referrer: 'no-referrer', // no-referrer, *client
-            })
-                .then(res => {
-                    if (res.status === 200) {
-                        return res.json()
-                    } else {
-                        window.alert('Väärä käyttäjätunnus tai salasana!')
-                    }
+            // remove login if somewhere is clicked
+            // document.getElementById('login-window').remove()
+
+            let rootLogin = document.createElement('div')
+            rootLogin.setAttribute('id', 'login-window')
+            document.body.appendChild(rootLogin)
+
+            let welcomeText = document.createElement('div')
+            welcomeText.innerHTML = 'Kirjaudu sisään'
+            welcomeText.setAttribute('class', 'login-welcome')
+
+            let formWrap = document.createElement('form')
+            formWrap.setAttribute('id', 'loginForm')
+
+            let userWrap = document.createElement('div')
+            userWrap.setAttribute('class', 'login-block')
+            let userField = document.createElement('input')
+            userField.setAttribute('type', 'text')
+            userField.setAttribute('name', 'username')
+            userField.setAttribute('placeholder', 'Käyttäjä')
+            userWrap.appendChild(userField)
+
+            let passWrap = document.createElement('div')
+            passWrap.setAttribute('class', 'login-block')
+            let passField = document.createElement('input')
+            passField.setAttribute('type', 'password')
+            passField.setAttribute('name', 'password')
+            passField.setAttribute('placeholder', 'Salasana')
+            passWrap.appendChild(passField)
+
+            let loginButton = document.createElement('input')
+            loginButton.setAttribute('type', 'submit')
+            loginButton.setAttribute('value', 'Lähetä')
+            loginButton.setAttribute('id', 'login-send')
+
+            let warning = document.createElement('div')
+            warning.setAttribute('class', 'login-warning')
+            warning.innerHTML = 'Väärä salasana tai käyttäjä!'
+
+            formWrap.onsubmit = event => {
+                event.preventDefault()
+                const username = userField.value
+                const password = passField.value
+                const credentials = username + ':' + password
+                const encodedCredentials = window.btoa(credentials)
+                fetch('/auth/login/helsinki', {
+                    // method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                    mode: 'cors', // no-cors, *cors, same-origin
+                    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                    credentials: 'same-origin', // include, *same-origin, omit
+                    headers: {
+                        Authorization: 'Basic ' + encodedCredentials,
+                    },
+                    redirect: 'follow', // manual, *follow, error
+                    referrer: 'no-referrer', // no-referrer, *client
                 })
-                .then(res => {
-                    if (res.apikey) {
-                        localStorage.setItem('apikey', res.apikey)
-                        window.alert('Tervetuloa admin!')
-                        window.location.reload()
-                    }
-                })
+                    .then(res => {
+                        if (res.status === 200) {
+                            return res.json()
+                        } else {
+                            formWrap.appendChild(warning)
+                        }
+                    })
+                    .then(res => {
+                        if (res.apikey) {
+                            localStorage.setItem('apikey', res.apikey)
+                            window.location.reload()
+                        }
+                    })
+            }
+
+            rootLogin.appendChild(welcomeText)
+            formWrap.appendChild(userWrap)
+            formWrap.appendChild(passWrap)
+            formWrap.appendChild(loginButton)
+            rootLogin.appendChild(formWrap)
+
+            // Factory approach:
+            // let labelTexts = ['username', 'password']
+
+            // const elementFactory = (type, elements) => {
+            //     elements.map(elem => {
+            //         let element = document.createElement(type)
+            //         element.innerHTML = elem
+            //         rootLogin.appendChild(element)
+            //     })
+            // }
+
+            // elementFactory('label', labelTexts)
         }).then(() => {
             return dispatch(retrieveUserFromSession())
         })
