f5acb7d4135cd7ad2a01b3068fba65ba6a9a321b
Lauri Nuotio <lauri.nuotio@siili.com>
2019-10-22 11:37:00 +0300

    fix: disable jwt usage, touch up on new login management

--- src/actors/serializer.js
+++ src/actors/serializer.js
@@ -1,20 +1,19 @@
-
 /*
 Saves store state to global module for another app
  */
 
-let arg = {};
+let arg = {}
 
-window.ARG = arg;
+window.ARG = arg
 
-var jwtDecode = require('jwt-decode');
+var jwtDecode = require('jwt-decode')
 
-export default (store) => {
-    const state = _.cloneDeep(_.omit(store.getState(), ['userEvents']));
-    if (state.user) {
-        let token = jwtDecode(state.user.token);
-        state.user.token = null; // JWT token is of not to be saved into Sentry
-        state.user.exp = token.exp;
-    }
-    window.ARG = state;
+export default store => {
+    // const state = _.cloneDeep(_.omit(store.getState(), ['userEvents']))
+    // if (state.user) {
+    //     let token = jwtDecode(state.user.token)
+    //     state.user.token = null // JWT token is of not to be saved into Sentry
+    //     state.user.exp = token.exp
+    // }
+    // window.ARG = state
 }
